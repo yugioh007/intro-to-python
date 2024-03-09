f990db073fda9f1cfa9b233f62005760c3633058
yugioh007 <160663929+yugioh007@users.noreply.github.com>
2024-03-09 21:49:42 +0100

    Week 1 assibnment

---
+++ user_input.py
@@ -0,0 +1,54 @@
+#Use the input() function to ask the user for their name and store it in a variable called "name"
+#name = input('What is your name? ')
+#print('My name is',name)
+#print(name)
+
+"""""
+name = input("What is your name?: ")
+print("Hello,", name,"Nice to meet you.")
+
+#Use the input() function to ask the user for their age and store it in a variable called "age".
+age = input('How old are you? ')
+print('Hi, I am', age, 'years old.')
+
+#Use the input() function to ask the user for their location and store it in a variable called "location"
+location = input("Where are you from? ")
+print("I am from", location)
+"""""
+
+#Using the user's name, age, and location. For example: "Hello [name], you are [age] years old and live in [location]."
+#name = "Ugo"
+# age = 30
+# location = "Ajah"
+# result = "Hello", name + ",", "you are", age, "years old and live in", location + "."
+# print(result)
+
+#name = input("Please enter your name: ")
+#age = input("Please enter your age: ")
+#location = input("Please enter your location: ")
+#result = "Hello", name + ",", "you are", age, "years old and live in", location + "."
+#print(result)
+
+
+# name = "Ugo"  # User's name
+# age = 30  # User's age
+# location = "Ajah, Lagos"  # User's location
+
+# # Create the greeting message
+# greeting = f"Hello {name}, you are {age} years old and live in {location}."
+# print(greeting)
+# x = {}
+# x[2] = 10
+# x[1] = [20,30,40]
+# print(x[1][2])
+# name = input("Please enter your name: ")
+# age = input("Please enter your age: ")
+# location = input("Please enter your location: ")
+# result = "Hello", name + ",", "you are", age, "years old and live in", location + "."
+# print(result)
+
+name = "Ugo"
+age = 30
+location = "Lagos, Nigeria"
+result = "Hello", name + ",", "you are", age, "years old and live in", location + "."
+print(result)
